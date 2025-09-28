be335c1a4ac4263c90d03e36b80ac0c8ed04fb31
streuselschnecke42 <michelle.weber201@gmail.com>
2025-09-28 22:20:18 +0200

    Task 3 done but could get improvement

--- ass2/task3/sdklink.c
+++ ass2/task3/sdklink.c
@@ -7,12 +7,12 @@
 
 void link_gpio_set_dir(int pin, int dir)
 {
-	gpio_set_dir(pin, dir);
+  gpio_set_dir(pin, dir);
 }
 
 void link_gpio_put(int pin, int value)
 {
-		gpio_put(pin, value);
+  gpio_put(pin, value);
 }
 
 int get_binary(int value, int position)
