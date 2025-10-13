3fd64f65d4ed13957517d369122c1a4e721e6dd1
streuselschnecke42 <michelle.weber201@gmail.com>
2025-10-13 17:59:50 +0200

    changed configuration leds (still errors: value too big line 32, 36; unshifted values needed in TST commands)

--- ass3/task3/sdklink.c
+++ ass3/task3/sdklink.c
@@ -7,17 +7,17 @@
 
 void link_gpio_set_dir(int pin, int dir)
 {
-	gpio_set_dir(pin, dir);
+    gpio_set_dir(pin, dir);
 }
 
 void link_gpio_put(int pin, int value)
 {
-	gpio_put(pin, value);
+    gpio_put(pin, value);
 }
 
 int link_gpio_get(int pin)
 {
-	return gpio_get(pin);
+    return gpio_get(pin);
 }
 
 void link_gpio_put_all(int value)
@@ -34,5 +34,5 @@ void link_gpio_put_all(int value)
 
 void link_gpio_pull_up(int pin)
 {
-	gpio_pull_up(pin);
+    gpio_pull_up(pin);
 }
