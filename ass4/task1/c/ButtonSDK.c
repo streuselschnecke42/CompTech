83b6ad657b7d36564c168112ebd637b7adf0993d
streuselschnecke-42 <michelle.weber201@gmail.com>
2025-10-23 17:47:34 +0200

    sent task 1c for testing

--- ass4/task1/c/ButtonSDK.c
+++ ass4/task1/c/ButtonSDK.c
@@ -8,6 +8,7 @@
 #include "pico/stdlib.h"
 
 const uint LED1 = 0;
+const uint LED2 = 6;
 const uint BTN1 = 1;
 const uint BTN2 = 2;
 
@@ -34,18 +35,21 @@ int gpioget(uint inputPin) {
 	return bit_inputPin;
 }
 
-void turnOn(uint outputPin) {
-	// SHIFT TO PIN POSITION TO GET BITMASK
-	int bitmask = (1 << outputPin);
+void turnOn(uint outputPin1, uint outputPin2) {
+	// SHIFT TO PIN POSITION TO GET BITMASK OF BOTH PINS
+	// (1 << outputPin1) = BITMASK FIRST PIN
+	// (1 << outputPin1) = BITMASK SECOND PIN
+	// | = BITWISE OR; COMBINES BOTH BITMASKS INTO ONE 32-BIT VALUE
+	int bitmask = (1 << outputPin1) | (1 << outputPin2);
 
 	// 0x014 is SIO_OUT_SET
 	// TRANSLATION TO: STR R3, [R2, #SIO_GPIO_OUT_SET_OFFSET]
 	*(SIO + 0x014 / 4) = bitmask;
 }
 
-void turnOff(uint outputPin) {
+void turnOff(uint outputPin, uint outputPin2) {
 	// SHIFT TO PIN POSITION TO GET BITMASK
-	int bitmask = (1 << outputPin);
+	int bitmask = (1 << outputPin) | (1 << outputPin2);
 
 	// 0x018 is SIO_OUT_CLR
 	// TRANSLATION TO: STR R3, [R2, #SIO_GPIO_OUT_CLR_OFFSET]
@@ -55,9 +59,13 @@ void turnOff(uint outputPin) {
 int main() {
 	stdio_init_all();
 	
-	// INITIALIZE LED
+	// INITIALIZE LED1
 	gpio_init(LED1);
 	gpio_set_dir(LED1, GPIO_OUT);
+
+	// INITIALIZE LED2
+	gpio_init(LED2);
+	gpio_set_dir(LED2, GPIO_OUT);
 	
 	// INITIALIZE BUTTON 1
 	gpio_init(BTN1);
@@ -74,11 +82,11 @@ int main() {
 	while (true) {
 		
 		if (gpioget(BTN1) == 0) {
-			turnOn(LED1);
+			turnOn(LED1, LED2);
 		}
 		
 		if (gpio_get(BTN2) == 0) {
-			turnOff(LED1);
+			turnOff(LED1, LED2);
 		}
 	}
 		
