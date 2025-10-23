5c89a6c4d02e38e882e797d4184182f225586a32
streuselschnecke-42 <michelle.weber201@gmail.com>
2025-10-23 16:00:10 +0200

    completed missing code in gpioget + added comments

--- ass4/task1/b/ButtonSDK.c
+++ ass4/task1/b/ButtonSDK.c
@@ -29,6 +29,8 @@ int main() {
 	gpio_pull_up(BTN2);
 	
 	// POINTER TO SIO BASE
+	// TRANSLATION TO: LDR R2, gpiobase
+	//				   gpiobase being #SIO_BASE
 	uint32_t *SIO = (uint32_t *)0xd0000000;
 
 	while (true) {
@@ -47,10 +49,20 @@ int main() {
 
 int gpioget(uint inputPin) {
 	// if uint doesnt work: try __u_int
-	// SHIFT TO PIN POSITION TO GET BITMASK
-	int bitmask = (1 << inputPin);
 
-	*(SIO + 0x004 / 4) = bitmask;
+	// TRANSLATION TO: LDR R3 ,[R2, #SIO_GPIO_IN_OFFSET]
+	// "GET GPIO IN VALUES (THE WHOLE 32 BIT THING)"
+	uint32_t gpioin = *(SIO + 0x004 / 4);
+
+	// SHIFT POINTER TO INPUT PIN POSITION AND 
+	// RIGHT-SHIFT IT ALL THE WAY TO THE RIGHT
+	// "POS OF LEAST SIGNIFICANT BIT"
+	int shifted_to_pin = gpioin >> inputPin;
+	
+	// ZERO OUT ALL NON-RELEVANT PINS TO GET BIT WE WANT
+	int bit_inputPin = shifted_to_pin & 1;
+
+	return bit_inputPin;
 }
 
 void turnOn(uint outputPin) {
@@ -59,6 +71,7 @@ void turnOn(uint outputPin) {
 	int bitmask = (1 << outputPin);
 
 	// 0x014 is SIO_OUT_SET
+	// TRANSLATION TO: STR R3, [R2, #SIO_GPIO_OUT_SET_OFFSET]
 	*(SIO + 0x014 / 4) = bitmask;
 }
 
@@ -67,5 +80,6 @@ void turnOff(uint outputPin) {
 	int bitmask = (1 << outputPin);
 
 	// 0x018 is SIO_OUT_CLR
+	// TRANSLATION TO: STR R3, [R2, #SIO_GPIO_OUT_CLR_OFFSET]
 	*(SIO + 0x018 / 4) = bitmask;
 }
