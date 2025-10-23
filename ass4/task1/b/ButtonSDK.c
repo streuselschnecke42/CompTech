9b0b103942c6b4f5106af27dd4d5b9522e94a556
streuselschnecke42 <michelle.weber201@gmail.com>
2025-10-23 17:25:28 +0200

    task 1b done

--- ass4/task1/b/ButtonSDK.c
+++ ass4/task1/b/ButtonSDK.c
@@ -11,44 +11,13 @@ const uint LED1 = 0;
 const uint BTN1 = 1;
 const uint BTN2 = 2;
 
-int main() {
-	stdio_init_all();
-	
-	// INITIALIZE LED
-	gpio_init(LED1);
-	gpio_set_dir(LED1, GPIO_OUT);
-	
-	// INITIALIZE BUTTON 1
-	gpio_init(BTN1);
-	gpio_set_dir(BTN1, GPIO_IN);
-	gpio_pull_up(BTN1);
-	
-	// INITIALIZE BUTTON 2
-	gpio_init(BTN2);
-	gpio_set_dir(BTN2, GPIO_IN);
-	gpio_pull_up(BTN2);
-	
-	// POINTER TO SIO BASE
-	// TRANSLATION TO: LDR R2, gpiobase
-	//				   gpiobase being #SIO_BASE
-	uint32_t *SIO = (uint32_t *)0xd0000000;
-
-	while (true) {
-		
-		if (gpioget(BTN1) == 0) {
-			turnOn(LED1);
-		}
-		
-		if (gpio_get(BTN2) == 0) {
-			turnOff(LED1);
-		}
-	}
-		
-	return 0;
-}
+// POINTER TO SIO BASE
+// TRANSLATION TO: LDR R2, gpiobase
+//				   gpiobase being #SIO_BASE
+// VOLATILE MEANS ITS CHANING
+volatile uint32_t *SIO = (uint32_t *)0xd0000000;
 
 int gpioget(uint inputPin) {
-	// if uint doesnt work: try __u_int
 
 	// TRANSLATION TO: LDR R3 ,[R2, #SIO_GPIO_IN_OFFSET]
 	// "GET GPIO IN VALUES (THE WHOLE 32 BIT THING)"
@@ -67,7 +36,6 @@ int gpioget(uint inputPin) {
 
 void turnOn(uint outputPin) {
 	// SHIFT TO PIN POSITION TO GET BITMASK
-	// IF ERROR: maybe try 1ul instead of 1
 	int bitmask = (1 << outputPin);
 
 	// 0x014 is SIO_OUT_SET
@@ -83,3 +51,38 @@ void turnOff(uint outputPin) {
 	// TRANSLATION TO: STR R3, [R2, #SIO_GPIO_OUT_CLR_OFFSET]
 	*(SIO + 0x018 / 4) = bitmask;
 }
+
+int main() {
+	stdio_init_all();
+	
+	// INITIALIZE LED
+	gpio_init(LED1);
+	gpio_set_dir(LED1, GPIO_OUT);
+	
+	// INITIALIZE BUTTON 1
+	gpio_init(BTN1);
+	gpio_set_dir(BTN1, GPIO_IN);
+	gpio_pull_up(BTN1);
+	
+	// INITIALIZE BUTTON 2
+	gpio_init(BTN2);
+	gpio_set_dir(BTN2, GPIO_IN);
+	gpio_pull_up(BTN2);
+	
+
+
+	while (true) {
+		
+		if (gpioget(BTN1) == 0) {
+			turnOn(LED1);
+		}
+		
+		if (gpio_get(BTN2) == 0) {
+			turnOff(LED1);
+		}
+	}
+		
+	return 0;
+}
+
+
